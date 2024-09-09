303eff95199c86c730daaa1ee6ff16e2f78382db
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-09-10 00:47:02 +0300

    implement hilt di

--- app/src/main/java/com/thkox/homeai/presentation/ui/activities/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/thkox/homeai/presentation/ui/activities/welcome/WelcomeActivity.kt
@@ -6,21 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import com.thkox.homeai.presentation.ui.activities.welcome.screens.auth.LoginScreen
 import com.thkox.homeai.presentation.ui.theme.HomeAITheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WelcomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HomeAITheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text("SettingsActivity")
+                    LoginScreen()
                 }
             }
         }
