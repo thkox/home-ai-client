26e9a9d60d9dc7e6b994d116c48364b276e740fd
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-09-25 23:49:55 +0300

    if the user logged in and open the app again, start from the main activity

--- app/src/main/java/com/thkox/homeai/presentation/ui/activities/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/thkox/homeai/presentation/ui/activities/welcome/WelcomeActivity.kt
@@ -1,16 +1,14 @@
 package com.thkox.homeai.presentation.ui.activities.welcome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.thkox.homeai.presentation.ui.activities.main.MainActivity
+import com.thkox.homeai.presentation.ui.theme.HomeAITheme
 import com.thkox.homeai.data.sources.local.SharedPreferencesManager
 import com.thkox.homeai.presentation.navigation.WelcomeNavHost
-import com.thkox.homeai.presentation.ui.theme.HomeAITheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -22,23 +20,18 @@ class WelcomeActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val startDestination = if (sharedPreferencesManager.getBaseUrl().isNullOrEmpty()) {
-            "enterServerAddress"
-        } else {
-            "login"
-        }
 
-        setContent {
-            HomeAITheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
+        // Check if the user is already logged in
+        if (sharedPreferencesManager.getToken() != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            setContent {
+                HomeAITheme {
                     WelcomeNavHost(
-                        navController = navController,
-                        sharedPreferencesManager = sharedPreferencesManager,
-                        startDestination = startDestination
+                        navController = rememberNavController(),
+                        startDestination = "enterServerAddress",
+                        sharedPreferencesManager = sharedPreferencesManager
                     )
                 }
             }
