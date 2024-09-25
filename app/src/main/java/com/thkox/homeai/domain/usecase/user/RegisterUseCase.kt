00feb3364caa49e9668a561020047f1a230ba11f
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-09-25 20:13:13 +0300

    update validations in the registration form

--- app/src/main/java/com/thkox/homeai/domain/usecase/user/RegisterUseCase.kt
+++ app/src/main/java/com/thkox/homeai/domain/usecase/user/RegisterUseCase.kt
@@ -13,17 +13,35 @@ class RegisterUseCase(
         if (userRegistration.firstName.isBlank()) {
             return Resource.Error("First name cannot be empty", "firstName")
         }
+        if (!userRegistration.firstName.matches(Regex("^[a-zA-Z]+$"))) {
+            return Resource.Error("First name must contain only alphabetic characters", "firstName")
+        }
         if (userRegistration.lastName.isBlank()) {
             return Resource.Error("Last name cannot be empty", "lastName")
         }
+        if (!userRegistration.lastName.matches(Regex("^[a-zA-Z]+$"))) {
+            return Resource.Error("Last name must contain only alphabetic characters", "lastName")
+        }
         if (userRegistration.username.isBlank()) {
             return Resource.Error("Email cannot be empty", "email")
+        }
+        if (!userRegistration.username.matches(Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"))) {
+            return Resource.Error("Invalid email format", "email")
         }
         if (userRegistration.password.isBlank()) {
             return Resource.Error("Password cannot be empty", "password")
         }
         if (userRegistration.password.length < 8) {
             return Resource.Error("Password must be at least 8 characters long", "password")
+        }
+        if (!userRegistration.password.any { it.isDigit() }) {
+            return Resource.Error("Password must contain at least one number", "password")
+        }
+        if (!userRegistration.password.any { it.isUpperCase() }) {
+            return Resource.Error("Password must contain at least one uppercase letter", "password")
+        }
+        if (!userRegistration.password.any { "!@#$%^&*()-_=+[]{}|;:,.<>?/".contains(it) }) {
+            return Resource.Error("Password must contain at least one special character", "password")
         }
 
         val result = authRepository.register(userRegistration)
