8945c75dcf8a43426efe1645b6390321a5a995cf
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-10-01 13:22:30 +0300

    cleanup

--- app/src/main/java/com/thkox/homeai/presentation/ui/activities/main/screens/ProfileScreen.kt
+++ app/src/main/java/com/thkox/homeai/presentation/ui/activities/main/screens/ProfileScreen.kt
@@ -1,12 +1,16 @@
 package com.thkox.homeai.presentation.ui.activities.main.screens
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -17,7 +21,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
@@ -93,7 +96,7 @@ fun ProfileContent(
                     text = "Profile",
                     style = MaterialTheme.typography.headlineMedium,
 
-                )
+                    )
             }
 
             item {
@@ -161,7 +164,7 @@ fun ProfileContent(
             }
 
             item {
-                Divider()
+                HorizontalDivider()
             }
 
             item {
